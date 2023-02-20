42ee9aae9d44ea8d814c074e7ff07c7d2065772d
Salah S <ssalame7167@conestogac.on.ca>
2023-02-19 20:37:10 -0500

    Added names and additional metadata to all files.

--- PROG71020-Lab4-G19/Circle.h
+++ PROG71020-Lab4-G19/Circle.h
@@ -2,6 +2,23 @@
 #include "Shape.h"
 
 #define PI_VALUE 3.141519
+/*
+* Program description:
+*	This program prints shapes and drives vehicles using inheritance, and function overriting.
+*
+* Authors and responsibilities:
+*	This source file has been created, and structured by Salah Salame, and additional
+*	contributions (including function calls, comments, and other functionality) has
+*	been added by the rest of my team.
+*
+* General information:
+*	Names:		Salah Salame, Amr Belbeisi, Drasti Patel.
+*	Course:		PROG71020 - Object Oriented Programming.
+*	Instructor: Akrem El-Ghazal.
+*	Date:		Initially developed on Thursday, February 16, 2022.
+*	Delivery:	To be handed in by Sunday, February 19, 2023.
+*	Group:		Name = "DAS"; Number = 19;
+*/
 
 class Circle:public Shape
 {
--- PROG71020-Lab4-G19/Rectangle.h
+++ PROG71020-Lab4-G19/Rectangle.h
@@ -1,5 +1,22 @@
 #pragma once
 #include "Shape.h"
+/*
+* Program description:
+*	This program prints shapes and drives vehicles using inheritance, and function overriting.
+*
+* Authors and responsibilities:
+*	This source file has been created, and structured by Salah Salame, and additional
+*	contributions (including function calls, comments, and other functionality) has
+*	been added by the rest of my team.
+*
+* General information:
+*	Names:		Salah Salame, Amr Belbeisi, Drasti Patel.
+*	Course:		PROG71020 - Object Oriented Programming.
+*	Instructor: Akrem El-Ghazal.
+*	Date:		Initially developed on Thursday, February 16, 2022.
+*	Delivery:	To be handed in by Sunday, February 19, 2023.
+*	Group:		Name = "DAS"; Number = 19;
+*/
 class Rectangle:public Shape
 {
 private:
